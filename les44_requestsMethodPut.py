16a0fbf104358c40675b2f466ffecc907f35af4b
koval_i <koval.illia.mailbox@gmail.com>
2025-06-04 23:46:20 +0300

    les44 p1

--- les44_requestsMethodPut.py
+++ les44_requestsMethodPut.py
@@ -1,8 +1,35 @@
 import requests
 
-url="https://api.chucknorris.io/jokes/random"
-result=requests.get(url)
-assert 200==result.status_code
-print(f"Success operation.Get new joke!")
-# result.encoding='utf-8'#optional -- for encoding the result
-print(result.text)
+
+class TestNewLocation():
+    """Working with new location"""
+
+    def testCreateNewLocation(self):
+        """Creating  new location"""
+        base_url = "https://rahulshettyacademy.com"  # base url
+        key = "?key=qaclick123"  # params for requests
+        post_resource = "/maps/api/place/get/json"  # resource method POST
+
+        post_url = base_url + post_resource + key
+        print(post_url)
+        json_for_creation_new_location = {
+            "location": {
+                "lat": -38.383494,
+                "lng": 33.427362
+            },
+            "accuracy": 50,
+            "name": "Frontline house",
+            "phone_number": "(+91) 983 893 3937",
+            "address": "29, side layout, cohen 09",
+            "types": [
+                "shoe park",
+                "shop"
+            ],
+            "website": "http://google.com",
+            "language": "French-IN"
+        }
+        # result_post=requests.post(post_url)
+
+
+new_place=TestNewLocation()
+new_place.testCreateNewLocation()
