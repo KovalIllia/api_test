514b3fa3cc4405da9adb0729de16cc027b6d690e
koval_i <koval.illia.mailbox@gmail.com>
2025-06-04 10:38:28 +0300

    les44

--- les44_requestsMethodPut.py
+++ les44_requestsMethodPut.py
@@ -1,9 +1,8 @@
 import requests
-# Put
 
-# url="https://api.chucknorris.io/jokes/random"
-# result=requests.get(url)
-# assert 200==result.status_code
-# print(f"Success operation.Get new joke!")
-# # result.encoding='utf-8'#optional -- for encoding the result
-# print(result.text)
+url="https://api.chucknorris.io/jokes/random"
+result=requests.get(url)
+assert 200==result.status_code
+print(f"Success operation.Get new joke!")
+# result.encoding='utf-8'#optional -- for encoding the result
+print(result.text)
