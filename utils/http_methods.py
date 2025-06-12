627fb168509c9f448d31f7874ac2d4c85128040d
koval_i <koval.illia.mailbox@gmail.com>
2025-06-12 10:57:58 +0300

    les56 all

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,4 +1,6 @@
 import requests
+from utils import *
+from utils.logger import Logger
 
 """List of http methods"""
 
@@ -9,21 +11,29 @@ class Http_methods():
 
     @staticmethod
     def get(url):
+        Logger.add_request(url,method="GET") #після виклику в логах буде відображатись запис
         result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")  # після виклику в логах буде відображатись запис
         result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUT")  # після виклику в логах буде відображатись запис
         result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")  # після виклику в логах буде відображатись запис
         result = requests.delete(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
